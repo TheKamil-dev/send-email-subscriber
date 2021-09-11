601872a4a58d8f15dcc081525ee2727523de67fc
b2b-dev1-admin <ksb2bdev@gmail.com>
2021-09-11 18:32:42 +0530

    email template change

--- resources/views/email/post.blade.php
+++ resources/views/email/post.blade.php
@@ -1,3 +1,21 @@
-hi
-{{$post->title}}
-{{$post->description}}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Post | {{$post->title}}</title>
+</head>
+<body style="font-size: 16px; max-width:670px; padding: 12px;">
+    <h2>
+        {{$post->title}}
+    </h2>
+    <div style="padding: 12px 20px;">
+        {{$post->description}}
+    </div>
+    <footer>
+        {{config('app.name')}}
+    </footer>
+</body>
+</html>
